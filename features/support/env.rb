4fec5244b408847a4fe90cb66f32a2be8afafaa2
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2013-09-09 09:44:41 -0400

    added missing require

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 require 'rspec-expectations'
 require 'page-object'
 require 'require_all'
+require 'fig_newton'
 
 require_all 'lib'
 
