402b6698c55cfdd25d9e4374bfe2c4afc72fbfd3
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2013-09-09 06:14:26 -0400

    Added FigNewton to base project

--- lib/pages/home_page.rb
+++ lib/pages/home_page.rb
@@ -1,7 +1,7 @@
 class HomePage
   include PageObject
 
-  page_url 'http://puppies.herokuapp.com'
+  page_url FigNewton.baseurl
 
   divs(:name, class: 'name')
   buttons(:view_detail, value: 'View Details')
